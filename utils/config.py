ca0c719a8bf38fab6e75d6c65afff8d5579deec5
JevonYang <yangzijing08@gmail.com>
2020-12-27 00:44:26 +0800

    add config file

---
+++ utils/config.py
@@ -0,0 +1,18 @@
+import configparser
+
+cf = configparser.ConfigParser()
+cf.read("../config.ini")
+
+
+def tushare_token():
+    return cf.get("tushare", "token")
+
+
+def databases():
+    type = cf.get("database", "type")
+    host = cf.get("database", "host")
+    user = cf.get("database", "user")
+    password = cf.get("database", "password")
+    db = cf.get("database", "db")
+    charset = cf.get("database", "charset")
+    return '%s://%s:%s@%s/%s?charset=%s' % (type, user, password, host, db, charset)
