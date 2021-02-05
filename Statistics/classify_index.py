9c5e223c5fb9c781f5ae3fa8b7132b5632bc4b69
JevonYang <yangzijing08@gmail.com>
2021-02-05 10:13:02 +0800

    优化接口

--- Statistics/classify_index.py
+++ Statistics/classify_index.py
@@ -8,13 +8,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def get_classify_index(start="2017-12-31", end=datetime.datetime.now().strftime("%Y-%m-%d")):
+def get_classify_index(
+    start="2017-12-31", 
+    end=datetime.datetime.now().strftime("%Y-%m-%d"), 
+    freq="Week") -> pd.DataFrame:
     sw_index_spot_df = ak.sw_index_spot()
     result_index = pd.DataFrame()
     for code in sw_index_spot_df['指数代码']:
         sw_index_df = ak.sw_index_daily_indicator(index_code=code, start_date=start,
                                                   end_date=end,
-                                                  data_type="Month")
+                                                  data_type=freq)
         init_price = float(sw_index_df.iloc[-1].at['close'])
         sw_index_df['relate_price'] = sw_index_df['close'].astype('float').apply(
             lambda x: (x - init_price) / init_price)
@@ -24,9 +27,7 @@ def get_classify_index(start="2017-12-31", end=datetime.datetime.now().strftime(
 
 
 if __name__ == '__main__':
-    result = get_classify_index()
-
-    print(result)
+    result = get_classify_index(start="2019-12-31", freq="Day")
 
     result.head(5).T.plot()
     plt.legend(loc='best')
