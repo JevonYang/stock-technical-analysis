66114da368d1716feca19d80f25c0979835d05e1
JevonYang <yangzijing08@gmail.com>
2021-02-06 23:23:49 +0800

    code polish

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 cf = configparser.ConfigParser()
 cf.read("../config.ini")
@@ -17,3 +18,19 @@ def databases():
     db = cf.get("database", "db")
     charset = cf.get("database", "charset")
     return '%s://%s:%s@%s:%s/%s?charset=%s' % (db_type, user, password, host, port, db, charset)
+
+
+def project_dir(project_name=None):
+    """
+        获取当前项目根路径
+        :param project_name:
+        :return: 根路径
+        """
+    PROJECT_NAME = 'stock-technical-analysis' if project_name is None else project_name
+    project_path = os.path.abspath(os.path.dirname(__file__))
+    root_path = project_path[:project_path.find("{}\\".format(PROJECT_NAME)) + len("{}\\".format(PROJECT_NAME))]
+    return root_path
+
+
+if __name__ == '__main__':
+    print(project_dir())
